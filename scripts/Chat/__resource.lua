aefa8b846233e221f307dfb26830a2303e9373fc
NEP <tacoarecool29@gmail.com>
2018-09-11 13:19:37 -0500

    Created __resource.lua

---
+++ scripts/Chat/__resource.lua
@@ -0,0 +1,8 @@
+----------------------------------
+-- Chat Commands Script, Made by Code3RP's Dev Team--
+----------------------------------
+
+resource_manifest_version "87a0356b-4610-4f88-b6f1-f1ccf4547cda"
+
+
+server_script 'chatcommands.lua'
