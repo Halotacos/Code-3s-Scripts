3e443b5b5a93a6dd4a348e871d44da9c335c285a
[Dev-1] Dark <tacoarecool29@gmail.com>
2018-09-10 22:00:04 -0500

    removed unneeded file

--- scripts/__resource.lua
+++
@@ -1,8 +0,0 @@
-----------------------------------
--- Twitter Script, Made by Code3RP's Dev Team--
-----------------------------------
-
-server_script 'twitter.lua'
-
---Thanks to the comment by SaltyGrandpa 
--- <3 Much love to you 
