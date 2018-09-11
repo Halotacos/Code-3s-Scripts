7903698165fb95b8a53cfffbf2e25cd1dc3edf80
[Dev-1] Dark <tacoarecool29@gmail.com>
2018-09-10 20:08:45 -0500

    Rewrote Twitter script using RegisterCommand | Edited __resource.lua

--- scripts/twitter.lua
+++ scripts/twitter.lua
@@ -1,23 +1,35 @@
--- twitter By NEP 
---Based off of fuzzymannerz ooc mod (https://github.com/fuzzymannerz/ooc)
+--Code3RP Twitter script Rewritten with RegisterCommand
+--Version 1.4
 
+AddEventHandler('chatMessage', function(source, name, message)
+      if string.sub(message, 1, string.len("/")) ~= "/" then
+          local name = GetPlayerName(source)
+		TriggerClientEvent("sendProximityMessage", -1, source, name, message)
+      end
+      CancelEvent()
+  end)
 
-AddEventHandler('chatMessage', function(source, name, msg)
-	sm = stringsplit(msg, " ");
-	if sm[1] == "/twt" then
-		CancelEvent()
-		TriggerClientEvent('chatMessage', -1, "^5TWITTER^7 | @^2".. name, {214, 214, 214}, string.sub(msg,5))
-	end
-end)
+  --for suggestion in chat 
+  local twtChat = 'void'
+
+  
+  --Twitter Command 
+
+  
+  
+  RegisterCommand('twt', function(source, args, user)
+	TriggerClientEvent('chatMessage', -1, "^0^5Twitter^0 | ^7@^2" .. GetPlayerName(source) .. "^0", {214, 214, 214}, table.concat(args, " "))
+end, false)
+TriggerEvent('chat:addSuggestion', '/' ..twtChat , 'Sends a Twitter message in chat.')
 
 function stringsplit(inputstr, sep)
-    if sep == nil then
-        sep = "%s"
-    end
-    local t={} ; i=1
-    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
-        t[i] = str
-        i = i + 1
-    end
-    return t
+	if sep == nil then
+		sep = "%s"
+	end
+	local t={} ; i=1
+	for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+		t[i] = str
+		i = i + 1
+	end
+	return t
 end
