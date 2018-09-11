9cdecc931d05b3c72356e0c4ac067ce59ca8c825
NEP <tacoarecool29@gmail.com>
2018-09-11 16:37:38 -0500

    Update twitter.lua

--- scripts/twitter/twitter.lua
+++ scripts/twitter/twitter.lua
@@ -6,16 +6,8 @@ print("=======================")
 
 --Code3RP Twitter script Rewritten with RegisterCommand
 --Version 1.4
-  --for suggestion in chat 
-  local twtChat = 'twt'
+
   --Twitter Command 
   RegisterCommand('twt', function(source, args, user)
-	--TriggerClientEvent('chatMessage', -1, "^0^5Twitter^0 | ^7@^2" .. GetPlayerName(source) .. "^0", {214, 214, 214}, table.concat(args, " "))
-TriggerEvent("SyncAd")
+	TriggerClientEvent('chatMessage', -1, "^0^5Twitter^0 | ^7@^2" .. GetPlayerName(source) .. "^0", {214, 214, 214}, table.concat(args, " "))
 	end, false)
-TriggerEvent('chat:addSuggestion', '/' ..twtChat , 'Sends a Twitter message in chat.')
-
-RegisterServerEvent("SyncAd")
-AddEventHandler('SyncAd', function(inputText)
-	TriggerClientEvent('DisplayAd', -1, inputText)
-end)
