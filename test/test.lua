d842b9b9d765a02242f5c0cca1e50fe55ef754d2
NEP <tacoarecool29@gmail.com>
2018-11-26 15:05:56 -0600

    Update test.lua

--- test/test.lua
+++ test/test.lua
@@ -3,3 +3,10 @@ AddEventHandler('onClientGameTypeStart', function()
                 args = { 'Test!~' }
             })
     end)
+
+RegisterCommand('coords', function(source, args)
+    local playerPed = PlayerPedId() -- get the local player ped
+    local pos = GetEntityCoords(playerPed)
+                      TriggerEvent('chat:addMessage', {
+            args = { 'Coords are:' .. pos }
+        })
